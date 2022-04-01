a6fd0f63565bf62c7d124ade35f6d328ad8a027b
mazyn <gilmarfilho75@gmail.com>
2022-04-01 17:33:25 -0300

    feat(frontend): finish home screen styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public headingText = 'You are only limited by';
   public id = '';
 
   public onSubmit() {
     if (!this.id) return;
     console.log('Oh, hi Mark!');
+  }
+
+  public onButtonHoverEnter() {
+    document.getElementById('id-input')?.classList.add('input-hover');
+  }
+
+  public onButtonHoverLeave() {
+    document.getElementById('id-input')?.classList.remove('input-hover');
   }
 }
