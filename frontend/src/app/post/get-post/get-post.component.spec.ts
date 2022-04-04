ddb51ee10aa6d702c99b139dd3f78c045ca08b36
mazyn <gilmarfilho75@gmail.com>
2022-04-04 14:50:04 -0300

    test(frontend): fixing current basic tests

--- frontend/src/app/post/get-post/get-post.component.spec.ts
+++ frontend/src/app/post/get-post/get-post.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxMaskModule } from 'ngx-mask';
+import { ToastrModule } from 'ngx-toastr';
 
 import { GetPostComponent } from './get-post.component';
+import { SharedComponentsModule } from '../../shared/components/shared-components.module';
+import { PostService } from '../services/post.service';
 
 describe('GetPostComponent', () => {
   let component: GetPostComponent;
@@ -9,6 +18,17 @@ describe('GetPostComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GetPostComponent],
+      imports: [
+        CommonModule,
+        FlexLayoutModule,
+        FormsModule,
+        HttpClientModule,
+        NgxMaskModule.forRoot(),
+        SharedComponentsModule,
+        RouterTestingModule,
+        ToastrModule.forRoot(),
+      ],
+      providers: [PostService],
     }).compileComponents();
   });
 
@@ -20,5 +40,12 @@ describe('GetPostComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render heading correctly', () => {
+    const expectedText = 'YOU ARE ONLY LIMITED BY YOUR ID.';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.heading-title')?.textContent).toContain(expectedText);
   });
 });
